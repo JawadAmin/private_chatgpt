a5021b6ca87a152f9c1bccf97b7b588f473bddd0
Jawad Amin <jawad.amin@slalom.com>
2023-04-04 12:14:34 -0400

    Added Langchain Few Shot Learning template.

--- tutoring-search-engine/app.py
+++ tutoring-search-engine/app.py
@@ -3,6 +3,8 @@ import openai
 from openai.embeddings_utils import get_embedding, cosine_similarity
 import pandas as pd
 import numpy as np
+from langchain import FewShotPromptTemplate
+from langchain import PromptTemplate
 
 app = Flask(__name__)
 app.static_folder = 'static'
@@ -30,7 +32,7 @@ def getResponse(query):
     clustered_text_df = pd.read_csv('clustered_embeddings.csv')
     clustered_text_df['embedding'] = clustered_text_df['embedding'].apply(eval).apply(np.array)
     clustered_text_df["similarities"] = clustered_text_df['embedding'].apply(lambda x: cosine_similarity(x, question_vector))
-    sorted_embeddings = clustered_text_df.sort_values("similarities", ascending=False).head(3)
+    sorted_embeddings = clustered_text_df.sort_values("similarities", ascending=False).head(2)
 
     context = []
     for i, row in sorted_embeddings.iterrows():
@@ -39,13 +41,57 @@ def getResponse(query):
     text = "\n".join(context)
     context = text
     
-    system_prompt = f"""Answer the following question using only the context provided. Answer in the style of a financial analyst. If you don't know the answer for certain, say I don't know."""
-    user_prompt = f"""
-    Context:
-    {context}
+    # create our examples
+    examples = [
+        {
+            "query": "How are you?",
+            "answer": "I can't complain but sometimes I still do."
+        }, {
+            "query": "What is the law of demand?",
+            "answer": "The law of demand is that the higher the price, the lower the quantity demanded, and the lower the price, the higher the quantity demanded. It's like the law of gravity, but for economics."
+        }
+    ]
 
-     Q: {query}
-    A:"""
+    # create a example template
+    example_template = """
+    User: {query}
+    AI: {answer}
+    """
+
+    # create a prompt example from above template
+    example_prompt = PromptTemplate(
+        input_variables=["query", "answer"],
+        template=example_template
+    )
+
+    # now break our previous prompt into a prefix and suffix
+    # the prefix is our instructions
+    prefix = """The following are exerpts from conversations with an AI
+    economics tutor. The tutor is typically sarcastic and witty, producing
+    creative  and funny responses to the users questions. The tutor responds
+    using the context provided. 
+
+    Context: {context}
+
+    Here are some response examples: 
+    """
+    # and the suffix our user input and output indicator
+    suffix = """
+    User: {query}
+    AI: """
+
+    # now create the few shot prompt template
+    few_shot_prompt_template = FewShotPromptTemplate(
+        examples=examples,
+        example_prompt=example_prompt,
+        prefix=prefix,
+        suffix=suffix,
+        input_variables=["query", "context"],
+        example_separator="\n\n"
+    )
+
+    user_prompt = f"""{few_shot_prompt_template.format(query=query, context=context)}"""
+    system_prompt = f""""""
 
     response = openai.ChatCompletion.create(
         temperature=0.5,
